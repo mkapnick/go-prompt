75ff623826cc909f000a426a1a23eae092d199d4
Michael <michaelk@vimeo.com>
2017-09-01 13:03:55 -0400

    Update

--- _examples/agile-ticket-survey.go
+++ _examples/agile-ticket-survey.go
@@ -1,12 +1,16 @@
 package main
 
-import "github.com/mkapnick/go-prompt"
+import (
+	"fmt"
+	"os"
+)
 
 var communicationOptions = []string{
 	"Good - no issues",
 	"Mostly good - some miscommunication occurred along the way",
 	"Somewhat good - too many issues occurred",
 	"Not good - no one was talking to eachother",
+	"N/A",
 }
 
 var effortOptions = []string{
@@ -14,6 +18,7 @@ var effortOptions = []string{
 	"Mostly accurate - there were some things that came up that made this either more or less work",
 	"Somewhat accurate - didn't think through every possible use case for this ticket",
 	"Not accurate - completely miscalculated",
+	"N/A",
 }
 
 var organizationOptions = []string{
@@ -21,15 +26,24 @@ var organizationOptions = []string{
 	"Mostly organized - workflow was smooth with a hiccup",
 	"Somewhat organized - workflow could have been a lot smoother",
 	"Not organized - workflow was confusing",
+	"N/A",
 }
 
 func main() {
+
+	if _, err := os.Stat("/tmp/agileStats.txt"); err != nil {
+		file, err := os.Create("/tmp/agileStats.txt")
+		if err != nil {
+			fmt.Println("Could not create /tmp/agileStats.txt file")
+			panic(err)
+		}
+	}
+
+	println(file)
+
+	/*ticket := prompt.String("Ticket reference number (CW-112):")
 	i := prompt.Choose("How well was communication on this ticket? ", communicationOptions)
-	println("==> You chose: " + communicationOptions[i])
-
 	j := prompt.Choose("How accurate was the effort on this ticket? ", effortOptions)
-	println("==> You chose: " + effortOptions[j])
-
 	k := prompt.Choose("How organized was the workflow of this ticket? ", organizationOptions)
-	println("==> You chose: " + organizationOptions[k])
+	*/
 }
